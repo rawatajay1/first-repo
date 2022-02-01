4757265b375944d749db2c303c2cb2ebfe67c62e
MeghaPurohit99 <98731103+MeghaPurohit99@users.noreply.github.com>
2022-02-01 12:43:40 +0530

    I have update scanner values
    added user input

--- add.java
+++ add.java
@@ -1,9 +1,18 @@
-class add
-{
-	public static void main(String args[])
-	{
-		int a=1,b=2;
-		int c=a+b;
-		System.out.print(c);
-	}
+import java.util.Scanner;
+public class add {
+
+    public static void main(String[] args) {
+        
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 }
